2b43279b3af1fe37e9b10438540c7c898cebba71
Uzair Minhas <uziminhas@gmail.com>
2023-04-20 14:44:14 -0400

    document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,28 @@ export default class MyDocument extends Document {
     return (
       <Html className={this.getTheme()}>
         <Head />
-
+          <meta name="title" content="Texas Eclipse Glasses" />
+          <meta name="description" content="Certified eclipse glasses to view the 2023 and 2024 solar eclipse | Made in America." />
+          <meta property="og:site_name" content="texaseclipseglasses.com" />
+          <meta
+            property="og:description"
+            content="Certified eclipse glasses to view the 2023 and 2024 solar eclipse | Made in America."
+          />
+          <meta property="og:title" content="Texas Eclipse Glasses" />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:title" content="Texas Eclipse Glasses" />
+          <meta
+            property="twitter:description"
+            content="Certified eclipse glasses to view the 2023 and 2024 solar eclipse | Made in America."
+          />
+          <meta
+            property="og:image"
+            content="https://texaseclipseglasses.com/preview.png"
+          />
+          <meta
+            property="twitter:image"
+            content="https://texaseclipseglasses.com/preview.png"
+          />
         <body>
           <Main />
           <NextScript />
