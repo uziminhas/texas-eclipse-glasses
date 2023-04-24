de765d5e4560f84f3a10d16a66db88af5470ccfe
Uzair Minhas <uziminhas@gmail.com>
2023-04-24 10:21:12 -0400

    logo add

--- src/core/ui/Logo/LogoImage.tsx
+++ src/core/ui/Logo/LogoImage.tsx
@@ -1,18 +1,12 @@
+import Image from 'next/image';
+
 const LogoImage: React.FCC<{
   className?: string;
 }> = ({ className }) => {
   return (
-    <h1
-      className="text-3xl xl:text-4xl font-bold cursor-pointer select-none"
-      style={{ fontFamily: "Brush Script MT" }}
-    >
-      <span>&#10029; </span>
-      <span className="text-transparent bg-clip-text bg-gradient-to-b from-blue-700 via-blue-700 to-red-500">
-        Texas Eclipse Glasses
-      </span>
-      <span> &#10029;</span>
-
-    </h1>
+    <div className={className}>
+      <Image src={`/assets/images/tex_logo.svg`} alt="Texas Eclipse Glasses" width={200} height={50} />
+    </div>
   );
 };
 
